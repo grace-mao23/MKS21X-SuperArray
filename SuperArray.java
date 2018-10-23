9de2a188b22a39980bae87eef58013b6e424a471
grace-mao23 <gmao00@stuy.edu>
2018-10-23 14:46:30 -0400

    Phase 1 coded, not yet tested

--- SuperArray.java
+++ SuperArray.java
@@ -17,6 +17,44 @@ public class SuperArray{
     return size == 0;
   }
 
+  public boolean add(String) {
+    data[size] = String;
+    size++;
+  }
 
+  public String toString() {
+    String result = "[ ";
+    for (int i = 0; i < size(); i++) {
+      result += data[i] + ", ";
+    }
+    result = result.substring(0,result.length() - 2);
+    result += "]";
+    return result;
+  }
+  public String toStringDebug() {
+    String result = "[ ";
+    for (int i = 0; i < data.length; i++) {
+      result += data[i] + ", ";
+    }
+    result = result.substring(0,result.length() - 2);
+    result += "]";
+    return result;
+  }
+
+  public String get(int i) {
+    if (i < 0 || i >= size()) {
+      return null;
+    } else {
+      return data[i];
+    }
+  }
+  public String set(int i, String str) {
+    if (i < 0 || i >= size()) {
+      return null;
+    } else {
+      data[i] = str;
+      return null;
+    }
+  }
 
 }
